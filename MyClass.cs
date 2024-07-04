a7fcb8ea4f1e99dfadba353654624e1c0d35eb05
Ahmedli Aqshin Araz <Ahmed_hb29@itstep.edu.az>
2024-07-04 15:19:20 +0400

    Event

---
+++ MyClass.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Event_Extension_Method_Standart_Delegate;
+
+public class MyClass
+{
+    public string Space(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        return new string(charArray);
+    }
+    public string Reverse(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using Event_Extension_Method_Standart_Delegate;
+
+MyClass myClass = new MyClass();
+//string reversed = myClass.Reverse("Hello");
+//Console.WriteLine(reversed);
+string original = "Hello";
+string spaced = myClass.Space(original);
+Console.WriteLine(spaced);
